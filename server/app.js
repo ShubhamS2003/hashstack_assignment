e768023bc24879c37695aa5e83a4d82db5d74710
ShubhamS2003 <sandhyayamlur1@gmail.com>
2025-01-10 16:36:30 +0530

    Node server at localhost:3000

--- server/app.js
+++ server/app.js
@@ -1,31 +1,24 @@
 const express = require('express');
 const blockRoutes = require('./routes/blockRoutes');
-
-// const fetchBlockDetails = async () => {
-//     try {
-//         console.log('Attempting to subscribe to new block headers...');
-//         const subscription = await web3.eth.subscribe("newBlockHeaders", function (error, result) {
-//             console.log("I reached");
-//             console.log(result);
-//             if (error) {
-//                 console.error("Error", error);
-//             }
-//         })
-//     } catch (error) {
-//         console.error('Error fetching block details:', error.message);
-//     }
-// }
+const accountRoutes = require('./routes/accountRoutes');
+const tokenRoutes = require('./routes/tokenRoutes');
+const blockHelper = require('./blockHelper');
+require("dotenv").config();
 
 
+// Update the database each time the server is turned on
+// blockHelper.storeBlockDetails();
+
+// Update the database each time a new block is mined on the chain
+// blockHelper.fetchLatestBlock();
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/api/v1/blocks', blockRoutes);
-
-// fetchBlockDetails();
-
+app.use('/api/v1/accounts', accountRoutes);
+app.use('/api/v1/tokens', tokenRoutes);
 
 
 app.listen(3000, () => {
